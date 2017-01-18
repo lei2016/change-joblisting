0c4b695e286adaf8cd4817cbddead0e3160aa509
Lei2016 <38833433@qq.com>
2017-01-18 20:16:23 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,16 @@ Rails.application.routes.draw do
    resources :resumes
  end
 
-  namespace :admin do
-    resources :jobs
-  end
+ namespace :admin do
+   resources :jobs do
+     member do
+       post :publish
+       post :hide
+     end
+
+     resources :resumes
+   end
+ end
   namespace :admin do
     resources :jobs do
       member do
